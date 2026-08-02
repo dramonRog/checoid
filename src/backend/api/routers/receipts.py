e91a4eb8181542384859a8ac090a6abbe6899801
Roman Buchynskyi <168230669+dramonRog@users.noreply.github.com>
2026-08-02 21:53:35 +0200

    feat(receipts): implement local file storage and eager load db relations

--- src/backend/api/routers/receipts.py
+++ src/backend/api/routers/receipts.py
@@ -1,14 +1,19 @@
-from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
 from src.backend.db.database import get_db
-from src.backend.db.models import User
+from src.backend.db.models import User, Receipt
 from src.backend.api.deps import get_current_user
 from src.backend.core.storage import save_upload_file
 
+from src.backend.schemas import ReceiptResponse
+
 router = APIRouter(prefix="/receipts", tags=["Receipts"])
 
-@router.post("/upload")
+@router.post("/upload", response_model=ReceiptResponse, status_code=status.HTTP_201_CREATED)
 async def upload_receipt_image(
     file: UploadFile = File(...),
     current_user: User = Depends(get_current_user),
@@ -24,8 +29,24 @@ async def upload_receipt_image(
 
     file_url = await save_upload_file(file)
 
-    return {
-        "status": "success",
-        "original_filename": file.filename,
-        "file_url": file_url
-    }
+    new_receipt = Receipt(
+        user_id=current_user.id,
+        image_url=file_url,
+        status="PROCESSING"
+    )
+
+    db.add(new_receipt)
+    await db.commit()
+
+    query = (
+        select(Receipt)
+        .where(Receipt.id == new_receipt.id)
+        .options(
+            selectinload(Receipt.items),
+            selectinload(Receipt.company)
+        )
+    )
+    result = await db.execute(query)
+    full_receipt = result.scalars().one()
+
+    return full_receipt
